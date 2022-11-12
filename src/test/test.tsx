03c2b536c10b9456d7497de52dee62cf14568df1
vova <vovachervinskii2905@gmail.com>
2022-11-12 22:58:09 +0600

    commit-4

---
+++ src/test/test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Test = () => {
+    return (
+    <div>
+        <h2>Test</h2>
+    </div>
+    )
+}
---
+++ src/user/test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Test = () => {
+    return (
+        <div>
+            <h2>User444</h2>
+        </div>
+    )
+}
